d8b7d033e0c774fbe09d55f8b8c249c7e7311e48
0xrazumovsky <khnv.artur@gmail.com>
2025-12-28 12:37:59 +0300

    feat: main.ts basic structure

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { WsAdapter } from '@nestjs/platform-ws';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  app.useWebSocketAdapter(new WsAdapter(app));
+
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  });
+  await app.listen(3000);
+
+  console.log('App is running on http://localhost:3000');
 }
-bootstrap();
+
+bootstrap().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
